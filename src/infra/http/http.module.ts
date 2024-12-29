6233bfa58b85ad4df12258ca28bff40665c17ea2
eldanielsoares <danielsoares7717@gmail.com>
2024-12-29 13:19:36 -0300

    feat: update http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,15 +4,27 @@ import { CryptographyModule } from '../cryptography/cryptography.module'
 import { EnvModule } from '../env/env.module'
 import { CreateUserController } from './controllers/users/CreateUserController'
 import { CreateUserUseCase } from 'src/domain/user/application/usecases/create-user'
+import { AuthenticateUserController } from './controllers/users/AuthenticateUserController'
+import { AuthenticateUserUseCase } from 'src/domain/user/application/usecases/authenticate-user'
+import { FindUserByIdController } from './controllers/users/FindUserById'
+import { FindUserByIdUseCase } from 'src/domain/user/application/usecases/find-user-by-id'
+import { DeleteUserUseCase } from 'src/domain/user/application/usecases/delete-user'
+import { DeleteUserController } from './controllers/users/DeleteUserController'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, EnvModule],
   controllers: [
     CreateUserController,
+    AuthenticateUserController,
+    FindUserByIdController,
+    DeleteUserController,
     // AuthenticateController,
   ],
   providers: [
     CreateUserUseCase,
+    AuthenticateUserUseCase,
+    FindUserByIdUseCase,
+    DeleteUserUseCase,
     // UploadAttachmentUseCase,
   ],
 })
