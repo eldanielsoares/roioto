6429a288c5ed29ed8516e4b766f9db2e8f58ca1c
eldanielsoares <danielsoares7717@gmail.com>
2024-12-29 21:04:44 -0300

    feat: payments

--- src/infra/payment/payment.module.ts
+++ src/infra/payment/payment.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common'
+import { PrismaService } from '../database/prisma/prisma.service'
+import { Payments } from './payments'
+import { Payment } from 'src/domain/user/application/payments/payment'
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [PrismaService, { provide: Payment, useClass: Payments }],
+  exports: [Payment],
 })
 export class PaymentModule {}
--- src/infra/payment/payments.ts
+++ src/infra/payment/payments.ts
@@ -48,6 +48,7 @@ export class Payments implements Payment {
         ],
       },
     })
+
     return preferenceResponse.id
   }
   async processPayment(data: ProcessPayment): Promise<unknown> {
