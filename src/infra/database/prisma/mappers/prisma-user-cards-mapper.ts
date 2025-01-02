716e2bf90cc6e39e0deb369e2657a4837e86e801
eldanielsoares <danielsoares7717@gmail.com>
2025-01-02 00:50:22 -0300

    fix: user cards id

--- src/infra/database/prisma/mappers/prisma-user-cards-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-user-cards-mapper.ts
@@ -1,6 +1,6 @@
-import { Card, Prisma, UserCard as PrismaUserCard } from '@prisma/client'
+import { Prisma, UserCard as PrismaUserCard } from '@prisma/client'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { UserCard } from '@/domain/user/enterprise/entities/user-card'
+import { Card, UserCard } from '@/domain/user/enterprise/entities/user-card'
 
 type MapperResponse = PrismaUserCard & {
   card?: Card
@@ -10,27 +10,23 @@ export class PrismaUserCardsMapper {
   static toDomain(raw: MapperResponse): UserCard {
     return UserCard.create(
       {
-        deckId: new UniqueEntityID(raw.deckId),
-        userId: new UniqueEntityID(raw.userId),
-        cardId: new UniqueEntityID(raw.cardId),
+        deckId: new UniqueEntityID(raw.deckId).toString(),
+        userId: new UniqueEntityID(raw.userId).toString(),
+        cardId: new UniqueEntityID(raw.cardId).toString(),
         card: raw.card
           ? {
-              id: new UniqueEntityID(raw.card.id),
-              categoryId: new UniqueEntityID(raw.card.categoryId),
+              id: raw.card.id,
+              categoryId: raw.card.categoryId,
               description: raw.card.description,
               image: raw.card.image,
               weight: raw.card.weight,
               shots: raw.card.shots,
               isFree: raw.card.isFree,
               level: raw.card.level,
-              deckId: raw.card.deckId
-                ? new UniqueEntityID(raw.card.deckId)
-                : undefined,
+              deckId: raw.card.deckId || undefined,
               createdAt: raw.card.createdAt,
               updatedAt: raw.card.updatedAt,
-              packId: raw.card.packId
-                ? new UniqueEntityID(raw.card.packId)
-                : undefined,
+              packId: raw.card.packId || undefined,
             }
           : undefined,
       },
