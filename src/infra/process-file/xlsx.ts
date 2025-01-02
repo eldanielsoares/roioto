aa482079b31a284e8166a0384ae5edbf6a1d1a32
eldanielsoares <danielsoares7717@gmail.com>
2025-01-02 18:40:33 -0300

    feat: generic process file

--- src/infra/process-file/xlsx.ts
+++ src/infra/process-file/xlsx.ts
@@ -1,11 +1,10 @@
 import { ProcessFile } from '@/domain/cards/application/process-file/process-file'
-import { Card, CardProps } from '@/domain/cards/enterprise/entities/card'
 import { Injectable } from '@nestjs/common'
 import * as XLSX from 'xlsx'
 
 @Injectable()
 export class XlsxProcessFile implements ProcessFile {
-  async processFile(file: Buffer): Promise<CardProps[]> {
+  async processFile<T>(file: Buffer): Promise<T[]> {
     const workbook = XLSX.read(file, { type: 'buffer' })
 
     const sheetName = workbook.SheetNames[0]
@@ -13,6 +12,6 @@ export class XlsxProcessFile implements ProcessFile {
 
     const jsonData = XLSX.utils.sheet_to_json(sheet)
 
-    return jsonData as CardProps[]
+    return jsonData as T[]
   }
 }
