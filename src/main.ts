2a95065293b36540f54b8746624b78e9ecefbb0b
eldanielsoares <danielsoares7717@gmail.com>
2025-01-05 14:09:34 -0300

    feat: add temporary cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(helmet())
+  app.enableCors({ origin: '*' })
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
